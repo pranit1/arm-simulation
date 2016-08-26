91d2df25ea5f53ba7820218b5786dbef8979e2f5
pranit1 <pspranits10494@gmail.com>
2016-08-26 22:22:08 +0530

    Update arm.c

--- arm.c
+++ arm.c
@@ -3,45 +3,28 @@
 //#include<windows.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
-
 //#include <math.h>
-
-
-
 static int shoulder = 0, elbow = 0,wrist=0,finger=0.0,finger2=0.0,finger3=0.0,text=0.0;
 int i=0;
 char c[]="MARK I";
-
-
-
 
 void init(void)
 {
   glClearColor (0.0, 0.0, 0.0, 0.0);
   glShadeModel (GL_FLAT);
 }
-
 void display(void)
 {
    glClear (GL_COLOR_BUFFER_BIT);
-
-
-   //Shoulder
-   
    glPushMatrix();
    glTranslatef (-1.0, 0.0, 0.0);//establish a pivot point
    glRotatef ((GLfloat) shoulder, 0.0, 0.0, 1.0);//to pivot the cube
    glTranslatef (1.0, 0.0, 0.0);//translate back to centre of cube
-
    glPushMatrix();
-
    glScalef (2.0, 0.4, 1.0);//cube is scaled
    glutWireCube (1.0);//draw cube
    glColor3f(0.0,1.0,0.0);
    glPopMatrix();
-
-
-
 
 //Elbow
 /* to build elbow we need to move local co-ordinate system to next pivot point.Since co-ordinate is previously rotated,the x-axis is already oriented along the length of rotated arm.therefore translating along the x axis moves the local coordinate to next point.this can be done for indefinite number of segments
@@ -49,100 +32,73 @@ void display(void)
    glTranslatef (1.0, 0.0, 0.0);
    glRotatef ((GLfloat) elbow, 0.0, 0.0, 1.0);
    glTranslatef (1.0, 0.0, 0.0);
-
    glPushMatrix();
    glScalef (2.0, 0.4, 1.0);
    glutWireCube (1.0);
-
-
-
    glColor3f(1.0,0.0,0.0);
-
-
    glPopMatrix();
 
-
-
-
-
-
 //Wrist
-   
    glTranslatef (1.0, 0.0, 0.0);
-  glRotatef ((GLfloat) wrist, 0.0, 0.0, 0.0);
- glTranslatef (0.3, 0.0, 0.0);
-  glPushMatrix();
-  glScalef (0.6, 0.7, 0.8);
- glutWireCube (1.0);
- glPopMatrix();
- 
- 
+   glRotatef ((GLfloat) wrist, 0.0, 0.0, 0.0);
+   glTranslatef (0.3, 0.0, 0.0);
+   glPushMatrix();
+   glScalef (0.6, 0.7, 0.8);
+   glutWireCube (1.0);
+   glPopMatrix();
 //finger1
 /*For finger do the same as shoulder and elbow take the size of cube as 0.3 andscale it to shrink the size and add another segment to make it more realistic
 */
- glPushMatrix();
-
- glTranslatef (0.0, 0.3, 0.2);
+   glPushMatrix();
+   glTranslatef (0.0, 0.3, 0.2);
    glRotatef ((GLfloat) finger, 0.0, 0.0, 1.0);
-  glTranslatef (0.3, 0.0, 0.0);
+   glTranslatef (0.3, 0.0, 0.0);
    glPushMatrix();
    glScalef (1.5, 0.3, 0.2);
-  glutWireCube (0.3);
-  glPopMatrix();
+   glutWireCube (0.3);
+   glPopMatrix();
+   glTranslatef (0.3, 0.0, 0.0);
+   glRotatef ((GLfloat) finger, 0.0, 0.0, 1.0);
+   glTranslatef (0.2, 0.0, 0.0);
+   glPushMatrix();
+   glScalef (1.5, 0.3, 0.2);
+   glutWireCube (0.3);
+   glPopMatrix();
+   glTranslatef (0.3, 0.0, 0.0);
+   glRotatef ((GLfloat) finger, 0.0, 0.0, 1.0);
+   glTranslatef (0.2, 0.0, 0.0);
+   glPushMatrix();
+   glScalef (1.5, 0.3, 0.2);
+   glutWireCube (0.3);
+   glPopMatrix();
+   glPopMatrix();
 
-  glTranslatef (0.3, 0.0, 0.0);
-     glRotatef ((GLfloat) finger, 0.0, 0.0, 1.0);
-    glTranslatef (0.2, 0.0, 0.0);
-     glPushMatrix();
-     glScalef (1.5, 0.3, 0.2);
-    glutWireCube (0.3);
-    glPopMatrix();
-
-
-    glTranslatef (0.3, 0.0, 0.0);
-       glRotatef ((GLfloat) finger, 0.0, 0.0, 1.0);
-      glTranslatef (0.2, 0.0, 0.0);
-       glPushMatrix();
-       glScalef (1.5, 0.3, 0.2);
-      glutWireCube (0.3);
-      glPopMatrix();
-
-      glPopMatrix();
 //finger1
-
-
-
-
-
 /*finger2
 for finger 2 repeat the same procedure as above except translate its z axis to 0.4*/
-      glPushMatrix();
-
-      glTranslatef (0.0, 0.3, 0.4);
-        glRotatef ((GLfloat) finger2, 0.0, 0.0, 1.0);
-       glTranslatef (0.25, 0.0, 0.0);
-        glPushMatrix();
-        glScalef (1.5, 0.3, 0.2);
-       glutWireCube (0.25);
-       glPopMatrix();
-
-       glTranslatef (0.25, 0.0, 0.0);
-          glRotatef ((GLfloat) finger2, 0.0, 0.0, 1.0);
-         glTranslatef (0.2, 0.0, 0.0);
-          glPushMatrix();
-          glScalef (1.5, 0.3, 0.2);
-         glutWireCube (0.25);
-         glPopMatrix();
-
-
-         glTranslatef (0.25, 0.0, 0.0);
-            glRotatef ((GLfloat) finger2, 0.0, 0.0, 1.0);
-           glTranslatef (0.2,0.0, 0.0);
-            glPushMatrix();
-            glScalef (1.5, 0.3, 0.2);
-           glutWireCube (0.25);
-           glPopMatrix();
-           glPopMatrix();
+   glPushMatrix();
+   glTranslatef (0.0, 0.3, 0.4);
+   glRotatef ((GLfloat) finger2, 0.0, 0.0, 1.0);
+   glTranslatef (0.25, 0.0, 0.0);
+   glPushMatrix();
+   glScalef (1.5, 0.3, 0.2);
+   glutWireCube (0.25);
+   glPopMatrix();
+   glTranslatef (0.25, 0.0, 0.0);
+   glRotatef ((GLfloat) finger2, 0.0, 0.0, 1.0);
+   glTranslatef (0.2, 0.0, 0.0);
+   glPushMatrix();
+   glScalef (1.5, 0.3, 0.2);
+   glutWireCube (0.25);
+   glPopMatrix();
+   glTranslatef (0.25, 0.0, 0.0);
+   glRotatef ((GLfloat) finger2, 0.0, 0.0, 1.0);
+   glTranslatef (0.2,0.0, 0.0);
+   glPushMatrix();
+   glScalef (1.5, 0.3, 0.2);
+   glutWireCube (0.25);
+   glPopMatrix();
+   glPopMatrix();
 
 //finger2
 
@@ -150,50 +106,40 @@ for finger 2 repeat the same procedure as above except translate its z axis to 0
 
 /* for finger 3 do the same aa above except change its z axis to 0.6*/
     glPushMatrix();
+    glTranslatef (0.0, 0.3, 0.6);
+    glRotatef ((GLfloat) finger3, 0.0, 0.0, 1.0);
+    glTranslatef (0.2, 0.0, 0.0);
+    glPushMatrix();
+    glScalef (1.5, 0.3, 0.2);
+    glutWireCube (0.2);
+    glPopMatrix();
+    glTranslatef (0.2, 0.0, 0.0);
+    glRotatef ((GLfloat) finger3, 0.0, 0.0, 1.0);
+    glTranslatef (0.1, 0.0, 0.0);
+    glPushMatrix();
+    glScalef (1.5, 0.3, 0.2);
+    glutWireCube (0.2);
+    glPopMatrix();
+    glTranslatef (0.2, 0.0, 0.0);
+    glRotatef ((GLfloat) finger3, 0.0, 0.0, 1.0);
+    glTranslatef (0.1,0.0, 0.0);
+    glPushMatrix();
+    glScalef (1.5, 0.3, 0.2);
+    glRasterPos3f(0.5,0.0,0.0);
+    glRotatef((GLfloat)text,0.0,0.0,0.0);
+    printf("%d\t %d\n",elbow,text);
+    for(i=0;c[i]!='\0';i++)
+    {
+	glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,c[i]);
+    }
 
-        glTranslatef (0.0, 0.3, 0.6);
-          glRotatef ((GLfloat) finger3, 0.0, 0.0, 1.0);
-         glTranslatef (0.2, 0.0, 0.0);
-          glPushMatrix();
-          glScalef (1.5, 0.3, 0.2);
-         glutWireCube (0.2);
-         glPopMatrix();
-
-         glTranslatef (0.2, 0.0, 0.0);
-            glRotatef ((GLfloat) finger3, 0.0, 0.0, 1.0);
-           glTranslatef (0.1, 0.0, 0.0);
-            glPushMatrix();
-            glScalef (1.5, 0.3, 0.2);
-           glutWireCube (0.2);
-           glPopMatrix();
-
-
-           glTranslatef (0.2, 0.0, 0.0);
-              glRotatef ((GLfloat) finger3, 0.0, 0.0, 1.0);
-             glTranslatef (0.1,0.0, 0.0);
-              glPushMatrix();
-              glScalef (1.5, 0.3, 0.2);
-glRasterPos3f(0.5,0.0,0.0);
-glRotatef((GLfloat)text,0.0,0.0,0.0);
-printf("%d\t %d\n",elbow,text);
-for(i=0;c[i]!='\0';i++)
-{
-glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,c[i]);
-}
-
-             glutWireCube (0.2);
-             glPopMatrix();
-             glPopMatrix();
+    glutWireCube (0.2);
+    glPopMatrix();
+    glPopMatrix();
              
              //Finger3
-
-
-
-
-      glPopMatrix();
-
-
-   glutSwapBuffers();
+    glPopMatrix();
+    glutSwapBuffers();
 }
 
 void reshape (int w, int h)
